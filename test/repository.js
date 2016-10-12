65345b489620212d85c650daa514f365beb1b506
Dmitry Shirokov <deadrunk@gmail.com>
2016-10-12 15:17:06 +1100

    Update repository.js

--- test/repository.js
+++ test/repository.js
@@ -4,12 +4,12 @@ const repository = require('../lib/repository');
 describe('repository', () => {
   const expected = { user: 'foo', repo: 'bar' };
 
-  it('should work when package.repository is defined and protol is http(s)', () => {
+  it('should work when package.repository is defined and protocol is http(s)', () => {
     assert.deepEqual(repository({ repository: { url: 'https://github.com/foo/bar.git' } }), expected);
     assert.deepEqual(repository({ repository: { url: 'git+https://github.com/foo/bar.git' } }), expected);
   });
 
-  it('should work when package.repository is defined and protol is git', () => {
+  it('should work when package.repository is defined and protocol is git', () => {
     assert.deepEqual(repository({ repository: { url: 'git@github.com:foo/bar.git' } }), expected);
   });
 
