a03b7d879a54a9eceda85de430ef9bb28efbc04f
Grigory Baleevskiy <baleevskii@gmail.com>
2017-04-21 13:53:07 +1000

    Add tests

---
+++ test/commit-analyzer.test.js
@@ -0,0 +1,40 @@
+const _ = require('lodash');
+const assert = require('assert');
+const sinon = require('sinon');
+const commitAnalyzer = require('../lib/commit-analyzer');
+
+describe('commit-analyzer', () => {
+  let sandbox;
+
+  const stubGit = (commitType) => {
+    const commitHistory = `44f026c Add eslintrc
+      56c1d28 [${commitType}]: Add some fixes
+      83ce07a Extract cmd
+      94df78c [${commitType}]Update README.md
+      0f23f08 Update README.md`;
+    sandbox.stub(commitAnalyzer, 'getCommitHistory').callsFake((commitRange, cb) => cb(commitHistory));
+    sandbox.stub(commitAnalyzer, 'getLastTag').callsFake(cb => cb('1.2.3'));
+  };
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+  _.forOwn({
+    major: 'major',
+    minor: 'minor',
+    patch: 'patch',
+    '': 'minor',
+  }, (resultType, commitType) => {
+    it(`should call cb with ${resultType}`, (done) => {
+      stubGit(commitType);
+      commitAnalyzer.getVersionType((err, versionType) => {
+        assert.equal(versionType, resultType);
+        done();
+      });
+    });
+  });
+});
