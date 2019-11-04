6c62a297741963f7a6590904cdaabc46497b6bab
Cameron Chamberlain <git@camjc.com>
2019-11-04 21:54:49 +1100

    bump eslint config and use jest (#32)

--- test/commit-analyzer.test.js
+++ test/commit-analyzer.test.js
@@ -1,45 +1,44 @@
 const _ = require('lodash');
-const assert = require('assert');
 const sinon = require('sinon');
 
 const commitAnalyzer = require('../lib/commit-analyzer');
 
-describe('commit-analyzer', () => {
-  let sandbox;
+let sandbox;
 
-  const stubGit = commitType => {
-    const commitHistory = `44f026c Add eslintrc
+const stubGit = commitType => {
+  const commitHistory = `44f026c Add eslintrc
       56c1d28 [${commitType}]: Add some fixes
       83ce07a Extract cmd
       94df78c [${commitType}]Update README.md
       0f23f08 Update README.md`;
-    sandbox.stub(commitAnalyzer, 'getCommitHistory').callsFake((commitRange, cb) => cb(commitHistory));
-    sandbox.stub(commitAnalyzer, 'getLastTag').callsFake(cb => cb('1.2.3'));
-  };
+  sandbox.stub(commitAnalyzer, 'getCommitHistory').callsFake((commitRange, cb) => cb(commitHistory));
+  sandbox.stub(commitAnalyzer, 'getLastTag').callsFake(cb => cb('1.2.3'));
+};
 
-  beforeEach(() => {
-    sandbox = sinon.createSandbox();
-  });
+beforeEach(() => {
+  sandbox = sinon.createSandbox();
+});
 
-  afterEach(() => {
-    sandbox.restore();
-  });
-  _.forOwn(
-    {
-      '': null,
-      major: 'major',
-      minor: 'minor',
-      other: null,
-      patch: 'patch',
-    },
-    (resultType, commitType) => {
-      it(`should call cb with ${resultType}`, done => {
+afterEach(() => {
+  sandbox.restore();
+});
+_.forOwn(
+  {
+    '': undefined,
+    major: 'major',
+    minor: 'minor',
+    other: undefined,
+    patch: 'patch',
+  },
+  (resultType, commitType) => {
+    it(`should call cb with ${resultType}`, () => {
+      return new Promise(done => {
         stubGit(commitType);
         commitAnalyzer.getVersionType((err, versionType) => {
-          assert.equal(versionType, resultType);
+          expect(versionType).toEqual(resultType);
           done();
         });
       });
-    }
-  );
-});
+    });
+  },
+);
