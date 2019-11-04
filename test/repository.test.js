82ae88aca2f29d3821ee14b61d0a68ae2e7745a4
Cameron Chamberlain <git@camjc.com>
2019-11-04 18:02:46 +1100

    bump eslint config and use jest

---
+++ test/repository.test.js
@@ -0,0 +1,25 @@
+const repository = require('../lib/repository');
+
+const expected = { repo: 'bar', user: 'foo' };
+
+it('should work when package.repository is defined and protocol is http(s)', () => {
+  expect(repository({ repository: { url: 'https://github.com/foo/bar.git' } })).toEqual(expected);
+
+  expect(repository({ repository: { url: 'git+https://github.com/foo/bar.git' } })).toEqual(expected);
+});
+
+it('should work when package.repository is defined and protocol is git', () => {
+  expect(repository({ repository: { url: 'git@github.com:foo/bar.git' } })).toEqual(expected);
+});
+
+it('should work when package.homepage is only defined', () => {
+  expect(repository({ homepage: 'https://github.com/foo/bar' })).toEqual(expected);
+  expect(repository({ homepage: 'https://github.com/foo/bar#readme' })).toEqual(expected);
+});
+
+it('should work with dashed in repo name', () => {
+  expect(repository({ repository: { url: 'https://github.com/foo/bar-baz.git' } })).toEqual({
+    repo: 'bar-baz',
+    user: 'foo',
+  });
+});
