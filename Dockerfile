428d65c0134ffc037c8469f8e924fc6fb255a6c0
jheanly <james.heanly@gmail.com>
2021-01-22 10:15:59 +1100

    [major] Upgrading NodeJS to v12. (SP-472) (#93)
    * [major] Upgrading NodeJS to v12.
    
    * [major] Bump deps
    SP-472
    
    * [major] Fix linting issue due to eslint-config upgrade
    SP-472

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:lts
+FROM node:12
 
 WORKDIR /root/app
 
--- test/commit-analyzer.test.js
+++ test/commit-analyzer.test.js
@@ -5,14 +5,14 @@ const commitAnalyzer = require('../lib/commit-analyzer');
 
 let sandbox;
 
-const stubGit = commitType => {
+const stubGit = (commitType) => {
   const commitHistory = `44f026c Add eslintrc
       56c1d28 [${commitType}]: Add some fixes
       83ce07a Extract cmd
       94df78c [${commitType}]Update README.md
       0f23f08 Update README.md`;
   sandbox.stub(commitAnalyzer, 'getCommitHistory').callsFake((commitRange, cb) => cb(commitHistory));
-  sandbox.stub(commitAnalyzer, 'getLastTag').callsFake(cb => cb('1.2.3'));
+  sandbox.stub(commitAnalyzer, 'getLastTag').callsFake((cb) => cb('1.2.3'));
 };
 
 beforeEach(() => {
@@ -32,7 +32,7 @@ _.forOwn(
   },
   (resultType, commitType) => {
     it(`should call cb with ${resultType}`, () =>
-      new Promise(done => {
+      new Promise((done) => {
         stubGit(commitType);
         commitAnalyzer.getVersionType((err, versionType) => {
           expect(versionType).toEqual(resultType);
