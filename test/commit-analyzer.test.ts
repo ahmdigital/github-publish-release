5f0b7e1122269fdee2479b4a6490b6ae817b73f3
Sam West-Sooby <s.west.sooby@gmail.com>
2023-05-09 09:56:16 +1000

    [patch] fix semver ordering bug (#216)
    * [patch] fix semver ordering bug
    
    * [patch] fix lint issue
    
    * [patch] lint files

--- test/commit-analyzer.test.ts
+++ test/commit-analyzer.test.ts
@@ -1,13 +1,12 @@
-import _ from 'lodash';
 import getVersionType from '../src/lib/commit-analyzer';
 import * as getLastTag from '../src/lib/get-last-tag';
 import * as getCommitHistory from '../src/lib/get-commit-history';
 
-const stubGit = (commitType: string) => {
+const stubGit = (commitType: string, anotherCommitType: string) => {
   const commitHistory = `44f026c Add eslintrc
       56c1d28 [${commitType}]: Add some fixes
       83ce07a Extract cmd
-      94df78c [${commitType}]Update README.md
+      94df78c [${anotherCommitType}]Update README.md
       0f23f08 Update README.md`;
 
   jest.spyOn(getCommitHistory, 'getCommitHistory').mockResolvedValue(commitHistory);
@@ -18,33 +17,30 @@ beforeEach(() => {
   jest.resetAllMocks();
 });
 
-_.forOwn(
-  {
-    major: 'major',
-    minor: 'minor',
-    patch: 'patch',
-  },
-  (resultType, commitType) => {
-    it(`should return ${resultType}`, async () => {
-      stubGit(commitType);
-      const versionType = await getVersionType();
-      expect(versionType).toEqual(resultType);
-    });
+it.each(['major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch', 'prerelease', 'dev'])(
+  'should return %s',
+  async (commitType) => {
+    stubGit(commitType, 'dev');
+    const versionType = await getVersionType();
+    expect(versionType).toEqual(commitType);
   },
 );
 
-_.forOwn(
+it.each([
   {
-    '': new Error("Can't recognise the release type! Please make sure the repo follows the commit message standard."),
-    other: new Error(
+    error: new Error(
       "Can't recognise the release type! Please make sure the repo follows the commit message standard.",
     ),
+    key: '',
   },
-  (error, commitType) => {
-    it(`should throw an error for ${commitType}`, async () => {
-      stubGit(commitType);
-      const promise = getVersionType();
-      await expect(promise).rejects.toThrow(error);
-    });
+  {
+    error: new Error(
+      "Can't recognise the release type! Please make sure the repo follows the commit message standard.",
+    ),
+    key: 'other',
   },
-);
+])('should throw an error for $key', async ({ key, error }) => {
+  stubGit(key, key);
+  const promise = getVersionType();
+  await expect(promise).rejects.toThrow(error);
+});
