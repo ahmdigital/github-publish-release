66ccfa8917b70f7a69390e317f8e748342048099
Sam <s.west.sooby@gmail.com>
2023-05-09 09:43:52 +1000

    [patch] lint files

--- test/commit-analyzer.test.ts
+++ test/commit-analyzer.test.ts
@@ -1,7 +1,6 @@
-import _ from "lodash";
-import getVersionType from "../src/lib/commit-analyzer";
-import * as getLastTag from "../src/lib/get-last-tag";
-import * as getCommitHistory from "../src/lib/get-commit-history";
+import getVersionType from '../src/lib/commit-analyzer';
+import * as getLastTag from '../src/lib/get-last-tag';
+import * as getCommitHistory from '../src/lib/get-commit-history';
 
 const stubGit = (commitType: string, anotherCommitType: string) => {
   const commitHistory = `44f026c Add eslintrc
@@ -10,45 +9,37 @@ const stubGit = (commitType: string, anotherCommitType: string) => {
       94df78c [${anotherCommitType}]Update README.md
       0f23f08 Update README.md`;
 
-  jest
-    .spyOn(getCommitHistory, "getCommitHistory")
-    .mockResolvedValue(commitHistory);
-  jest.spyOn(getLastTag, "getLastTag").mockResolvedValue("1.2.3");
+  jest.spyOn(getCommitHistory, 'getCommitHistory').mockResolvedValue(commitHistory);
+  jest.spyOn(getLastTag, 'getLastTag').mockResolvedValue('1.2.3');
 };
 
 beforeEach(() => {
   jest.resetAllMocks();
 });
 
-it.each([
-  "major",
-  "minor",
-  "patch",
-  "premajor",
-  "preminor",
-  "prepatch",
-  "prerelease",
-  "dev",
-])("should return %s", async (commitType) => {
-  stubGit(commitType, 'dev');
-  const versionType = await getVersionType();
-  expect(versionType).toEqual(commitType);
-});
+it.each(['major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch', 'prerelease', 'dev'])(
+  'should return %s',
+  async (commitType) => {
+    stubGit(commitType, 'dev');
+    const versionType = await getVersionType();
+    expect(versionType).toEqual(commitType);
+  },
+);
 
 it.each([
   {
-    key: "",
     error: new Error(
-      "Can't recognise the release type! Please make sure the repo follows the commit message standard."
+      "Can't recognise the release type! Please make sure the repo follows the commit message standard.",
     ),
+    key: '',
   },
   {
-    key: "other",
     error: new Error(
-      "Can't recognise the release type! Please make sure the repo follows the commit message standard."
+      "Can't recognise the release type! Please make sure the repo follows the commit message standard.",
     ),
+    key: 'other',
   },
-])("should throw an error for $key", async ({ key, error }) => {
+])('should throw an error for $key', async ({ key, error }) => {
   stubGit(key, key);
   const promise = getVersionType();
   await expect(promise).rejects.toThrow(error);
